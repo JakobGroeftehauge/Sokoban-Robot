5e8d3363ee208fae41fa7a6c732cc065296324d1
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2020-10-07 09:58:37 +0200

    Implemented switch case for initialising different behaviours

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 from  utils.decoder import Decoder
 from  utils.behaviours import *
 
+# Definition of actions/behvaiours
+FORWARD = "f"
+LEFT = "l"
+RIGHT = "r"
+NOT_DEFINED = -1
+
+DEFINED_ACTIONS = [FORWARD, LEFT, RIGHT]
+
+
+
 def main():
     string_of_actions = "flfrl"
-    decoder = Decoder(string_of_actions)
+    decoder = Decoder(string_of_actions, DEFINED_ACTIONS)
 
     current_action = decoder.get_next_action()
 
-    while current_action != -1:
-       current_action = -1 
+    while current_action != NOT_DEFINED: # current action == -1 -> no more actions to execute
+        
+        if(current_action == FORWARD):
+            move_forward()
+            print(current_action)
+            current_action = decoder.get_next_action()
+            
 
-    print("test")
+        elif(current_action == LEFT): 
+            turn_left()
+            print(current_action)
+            current_action = decoder.get_next_action()
+            
+
+        elif(current_action == RIGHT): 
+            turn_right() 
+            print(current_action)
+            current_action = decoder.get_next_action()
+            
+            
+        else: 
+            raise TypeError("No switch case implemented for action/behaviour: {}".format(current_action))
+       
 
 
 
--- utils/decoder.py
+++ utils/decoder.py
@@ -1,18 +1,18 @@
 class Decoder:
-    def __init__(self, actions):
+    def __init__(self, actions, defined_actions):
         self.actions = self.expand_actions(actions)
         self.action_pointer = 0
-        self.conversion_table = {'f': 0, 'l':1, 'r':2} 
+        self.defined_actions = defined_actions
 
 
     def get_next_action(self):
         if(len(self.actions) > self.action_pointer):
             current_action = self.actions[self.action_pointer]
-            if current_action in self.conversion_table:
-                current_action = self.conversion_table[current_action]
-            else: 
-                raise TypeError("Action not defined")
             self.action_pointer += 1
+
+            if current_action not in self.defined_actions:
+                raise TypeError("Action {} not defined".format(current_action))
+
             return current_action
         else: 
             return -1 # No more actions to execute
@@ -22,11 +22,10 @@ class Decoder:
 
 
 def test_decoder():
-    decoder = Decoder("flrflr")
+    DEFINED_ACTIONS = ['l', 'r', 't']
+    decoder = Decoder("flrflr", DEFINED_ACTIONS)
     
-    test = 1 
-   
-    print(test)
+    test = 1
     while test != -1:
         test = decoder.get_next_action()
         print(test)

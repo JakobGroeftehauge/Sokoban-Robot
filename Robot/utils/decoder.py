72d8691785f9f62d678e15da9276f16cb0a17b2f
JakobGroeftehauge <jakob.groeftehauge@gmail.com>
2020-10-22 13:44:07 +0200

    Expansion of action string added to decoder

--- Robot/utils/decoder.py
+++ Robot/utils/decoder.py
@@ -17,14 +17,49 @@ class Decoder:
         else:
             return -1 # No more actions to execute
 
-    def expand_actions(self, actions):
-        return actions
+    def expand_actions(self, actions, initial_direction='u'):
+        expanded_list_actions = ""
+        current_direction = initial_direction
+        for i in range(len(actions)):  
+            # If actions is c. The can is being manipulated. take care of.  
 
+            if not current_direction == actions[i]: 
+                expanded_list_actions = expanded_list_actions + self.get_action(current_direction, actions[i])
+            expanded_list_actions = expanded_list_actions + 'f'
+            current_direction = actions[i]
+        
+        return expanded_list_actions
 
+    def get_action(self, current_direction, wanted_direction):
+        """
+        docstring
+        """
+        if current_direction == "d" and wanted_direction == "u" or current_direction == "u" and wanted_direction == "d":
+            return "rr"
+        elif current_direction == "r" and wanted_direction == "l" or current_direction == "l" and wanted_direction == "r":
+            return "rr" 
+        elif current_direction == "r" and wanted_direction == "u":
+            return "l"
+        elif current_direction == "r" and wanted_direction == "d":
+            return "r"
+        elif current_direction == "l" and wanted_direction == "u":
+            return "r"
+        elif current_direction == "l" and wanted_direction == "d":
+            return "l"
+        elif current_direction == "u" and wanted_direction == "r":
+            return "r"
+        elif current_direction == "u" and wanted_direction == "l":
+            return "l"
+        elif current_direction == "d" and wanted_direction == "l":
+            return "l"
+        elif current_direction == "d" and wanted_direction == "r":
+            return "r" 
+    
 def test_decoder():
-    DEFINED_ACTIONS = ['l', 'r', 't']
-    decoder = Decoder("flrflr", DEFINED_ACTIONS)
+    DEFINED_ACTIONS = ['l', 'r', 'u', 'd', 'f']
+    decoder = Decoder("llrrurur", DEFINED_ACTIONS)
 
+    print(decoder.actions)
     test = 1
     while test != -1:
         test = decoder.get_next_action()
